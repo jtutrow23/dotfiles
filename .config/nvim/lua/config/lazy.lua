1f9befc086c98943bc693584b0f5cf9740577831
Justin Tutrow <jtutrow23@gmail.com>
2025-10-22 12:57:28 -0400

    nvim: clean bootstrap with lazy.nvim; fix symlink loop

---
+++ .config/nvim/lua/config/lazy.lua
@@ -0,0 +1,20 @@
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git","clone","--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git","--branch=stable", lazypath
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+  spec = {
+    { "nvim-lua/plenary.nvim" },
+    { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+    { "nvim-telescope/telescope.nvim", dependencies = { "nvim-lua/plenary.nvim" } },
+  },
+  install = { colorscheme = { "habamax" } },
+  change_detection = { notify = false },
+  rocks = { enabled = false },
+})
