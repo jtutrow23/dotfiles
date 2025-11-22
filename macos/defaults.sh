bfdbccdb64d8deddde77054ac9b27982580f9c71
jtutrow23 <33502807+jtutrow23@users.noreply.github.com>
2025-11-21 22:20:17 -0500

    Update macOS defaults for Finder and Trackpad settings

--- macos/defaults.sh
+++ macos/defaults.sh
@@ -4,32 +4,58 @@ set -euo pipefail
 # Dock
 defaults write com.apple.dock persistent-apps -array "" && killall Dock
 
-# Finder
-defaults write com.apple.finder QuitMenuItem -bool true
+# Disable warning when changing a file extension
 defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
+
+# Show all files and their extensions
 defaults write com.apple.finder AppleShowAllExtensions -bool true
 defaults write com.apple.finder AppleShowAllFiles -bool true
+
+# Show path bar, and layout as multi-column
 defaults write com.apple.finder ShowPathbar -bool true
 defaults write com.apple.finder FXPreferredViewStyle -string clmv
+
+# Search in current folder by default
 defaults write com.apple.finder FXDefaultSearchScope -string SCcf
+
+# Show directories first
 defaults write com.apple.finder _FXSortFoldersFirst -bool true
+
+# New window use the $HOME path
 defaults write com.apple.finder NewWindowTarget -string PfHm
 defaults write com.apple.finder NewWindowTargetPath -string "file://$HOME/"
+
+# Allow text selection in Quick Look
 defaults write com.apple.finder QLEnableTextSelection -bool true
+
+# Show metadata info, but not preview in info panel
 defaults write com.apple.finder FXInfoPanesExpanded -dict MetaData -bool true Preview -bool false
 
-# Trackpad
-defaults write com.apple.AppleMultitouchTrackpad TrackpadThreeFingerDrag -bool true
-defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadThreeFingerDrag -bool true
+# Enable trackpad tap to click
 defaults write com.apple.AppleMultitouchTrackpad Clicking -bool true
 defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
 
+# Enable 3-finger drag
+defaults write com.apple.AppleMultitouchTrackpad TrackpadThreeFingerDrag -bool true
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadThreeFingerDrag -bool true
+
+
 # System
 defaults write com.apple.LaunchServices LSQuarantine -bool false
+
+# Avoid creating .DS_Store files on USB or network volumes
+defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+
+# Disable disk image verification
 defaults write com.apple.frameworks.diskimages skip-verify -bool true
 defaults write com.apple.frameworks.diskimages skip-verify-locked -bool true
 defaults write com.apple.frameworks.diskimages skip-verify-remote -bool true
-defaults write com.apple.CrashReporter DialogType -string none
+
+# Disable personalized advertising
+defaults com.apple.AdLib forceLimitAdTracking -bool true
+defaults com.apple.AdLib allowApplePersonalizedAdvertising -bool false
+defaults com.apple.AdLib allowIdentifierForAdvertising -bool false
 
 # Safari
 defaults write com.apple.Safari AutoOpenSafeDownloads -bool false
@@ -45,7 +71,6 @@ defaults write -g InitialKeyRepeat -int 10
 defaults write -g KeyRepeat -int 1
 
 # Scroll/NAV/Text
-defaults write -g com.apple.swipescrolldirection -bool false
 defaults write -g NSAutomaticDashSubstitutionEnabled -bool false
 defaults write -g NSAutomaticPeriodSubstitutionEnabled -bool false
 defaults write -g NSAutomaticQuoteSubstitutionEnabled -bool false
@@ -53,6 +78,4 @@ defaults write -g NSAutomaticCapitalizationEnabled -bool false
 defaults write -g NSNavPanelExpandedStateForSaveMode -bool true
 defaults write -g NSNavPanelExpandedStateForSaveMode2 -bool true
 defaults write -g NSWindowResizeTime -float 0.001
-defaults write -g NSDocumentSaveNewDocumentsToCloud -bool true
 defaults write -g AppleAccentColor -int 6
-defaults write -g AppleWindowTabbingMode -string always
