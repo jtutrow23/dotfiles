8c33fb8928b5212c69dba45e3c53b8ecdc4eb50d
jtutrow23 <33502807+jtutrow23@users.noreply.github.com>
2026-07-09 14:04:18 -0400

    Update defaults.sh with Finder settings and removals
    Removed auto hide menubar and keyboard access settings. Added various Finder preferences for file visibility, search scope, and desktop cleanliness.

--- defaults.sh
+++ defaults.sh
@@ -1,9 +1,3 @@
-# Auto hide the menubar
-defaults write -g _HIHideMenuBar -bool true
-
-# Enable full keyboard access for all controls
-defaults write -g AppleKeyboardUIMode -int 3
-
 # Enable press-and-hold repeating
 defaults write -g ApplePressAndHoldEnabled -bool false
 defaults write -g InitialKeyRepeat -int 10
@@ -39,6 +33,42 @@ defaults write com.apple.dock show-process-indicators -bool false
 # --- Unpin all apps
 defaults write com.apple.dock persistent-apps -array ""
 
+# Allow quitting via ⌘Q
+defaults write com.apple.finder QuitMenuItem -bool true
+
+# Disable warning when changing a file extension
+defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
+
+# Show all files and their extensions
+defaults write com.apple.finder AppleShowAllExtensions -bool true
+defaults write com.apple.finder AppleShowAllFiles -bool true
+
+# Show path bar, and layout as multi-column
+defaults write com.apple.finder ShowPathbar -bool true
+defaults write com.apple.finder FXPreferredViewStyle -string clmv
+
+# Search in current folder by default
+defaults write com.apple.finder FXDefaultSearchScope -string SCcf
+
+# --- Keep the desktop clean
+defaults write com.apple.finder ShowHardDrivesOnDesktop -bool false
+defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool false
+defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool false
+defaults write com.apple.finder ShowMountedServersOnDesktop -bool false
+
+# Show directories first
+defaults write com.apple.finder _FXSortFoldersFirst -bool true
+
+# New window use the $HOME path
+defaults write com.apple.finder NewWindowTarget -string PfHm
+defaults write com.apple.finder NewWindowTargetPath -string "file://$HOME/"
+
+# Allow text selection in Quick Look
+defaults write com.apple.finder QLEnableTextSelection -bool true
+
+# Show metadata info, but not preview in info panel
+defaults write com.apple.finder FXInfoPanesExpanded -dict MetaData -bool true Preview -bool false
+
 # Enable trackpad tap to click
 defaults write com.apple.AppleMultitouchTrackpad Clicking -bool true
 defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
@@ -67,5 +97,7 @@ defaults write com.apple.frameworks.diskimages skip-verify-remote -bool true
 
 # Disable crash reporter
 defaults write com.apple.CrashReporter DialogType -string none
+
+sudo spctl --master-disabledefaults write com.apple.CrashReporter DialogType -string none
 
 sudo spctl --master-disable
